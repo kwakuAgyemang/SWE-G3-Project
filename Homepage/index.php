0274a0ca96d1a2a23b29966c0a8ab46d08827381
kwakuAgyemang <59964154+kwakuAgyemang@users.noreply.github.com>
2021-02-22 10:48:01 +0000

    Adjusting the theme

---
+++ Homepage/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <title>BiblioTeca</title>
+    <link rel="stylesheet" href="homepageFooter.css">
+</head>
+<body>
+    <?php require_once "homepageHeader.php";?>
+
+    <div class="container container-fluid">
+        <h2 class="display-4 text-center">Ashesi</h2>
+        <h3 class="display-5 text-center">Online Library</h3>
+        <div class="input-group">
+            <input type="search" class="form-control rounded" placeholder="Search books, articles, journals, textbooks" aria-label="Search"
+              aria-describedby="search-addon" />
+            <button type="button" class="btn btn-outline-primary">search</button>
+        </div>
+    </div>
+    <footer>
+        <div class="main-content">
+            <h4>About Us</h4><br>
+            <h4>Contact Us</h4>
+        </div>
+    </footer>
+    
+
+    
+</body>
+</html>
