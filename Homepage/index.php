1753d4caed4e1b81cfd56a98f26299d08af0e144
kwakuAgyemang <59964154+kwakuAgyemang@users.noreply.github.com>
2021-02-23 03:33:42 +0000

    Updates for index page and creation of login page

--- Homepage/index.php
+++ Homepage/index.php
@@ -5,10 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <title>BiblioTeca</title>
-    <link rel="stylesheet" href="homepageFooter.css">
+    
+    <link rel="stylesheet" href="homepageHeaderstyle.css">
 </head>
 <body>
-    <?php require_once "homepageHeader.php";?>
+    <nav>
+        <div class="menu-icon">
+            <span class="fas fa-bars"></span>
+        </div>
+        <div class="logo">BiblioTeca</div>
+        <div class="nav-items">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Novels</a></li>
+            <li><a href="#">Textbooks</a></li>
+            <li><a href="#">Journals</a></li>
+            <li><a href="#">Articles</a></li>
+        </div>
+        <div class="search-icon">
+            <span class="fas fa-search"></span>
+        </div>
+        <div class="cancel-icon">
+            <span class="fas fa-times"></span>
+        </div>
+        <a name="login" id="login" class="btn btn-primary" href="/SWE-G3-Project/login.php" role="button">Login</a>
+    </nav>
 
     <div class="container container-fluid">
         <h2 class="display-4 text-center">Ashesi</h2>
@@ -19,13 +39,8 @@
             <button type="button" class="btn btn-outline-primary">search</button>
         </div>
     </div>
-    <footer>
-        <div class="main-content">
-            <h4>About Us</h4><br>
-            <h4>Contact Us</h4>
-        </div>
-    </footer>
     
+    <?php include_once "homepageFooter.php" ?>
 
     
 </body>
